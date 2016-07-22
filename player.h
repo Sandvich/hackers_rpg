0e67b54897c38dee02ebd8cfe15e6ea9d4eb1796
Sanchit Sharma <ssharma@vpt.co.uk>
2016-07-22 13:01:56 +0100

    Changed a bunch of private to protected, as I misunderstood the difference between them!

--- player.h
+++ player.h
@@ -5,14 +5,12 @@
 #define __PLAYER_H_INCLUDED__
 
 class Player {
+protected:
 	int function_count; //For remembering which function we call next
 	bool modify_integrity(int modifier);
 	/* Most player-defined functions should go here,
 	 * This is because they should not be accesible by other objects - said other objects will
 	 * be other entites within a given battle.*/
-protected:
-	/* I haven't yet decided on all of these.
-	 * More may be added or taken away in the future. */
 	std::string name;
 	int integrity;
 	int initiative;
