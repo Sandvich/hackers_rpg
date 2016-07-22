f8be95d9505872609056c7ead1e5c6c40b9b478f
Sanchit Sharma <ssharma@vpt.co.uk>
2016-07-22 03:55:23 +0100

    Added in integrity, initiative and name.

--- player.cpp
+++ player.cpp
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "player.h"
 
+player::modify_integrity(int modifier) {
+	this->integrity = this->integrity + modifier
+	if (this->integrity <= 1) {
+		return false
+	} else {
+		return true
+	}
+};
+
 /* A basic player object.
  * The exact contents are flexible, but certain things are required.
  * Players will make their own player objects by using this class as a base. */
--- player.h
+++ player.h
@@ -1,15 +1,21 @@
 /* Player H file. */
+#include <string>
 
 #ifndef __PLAYER_H_INCLUDED__
 #define __PLAYER_H_INCLUDED__
 
 class player() {
 	int function_count = 0; //For remembering which function we call next
+	bool modify_integrity(int modifier);
 	/* Most player-defined functions should go here,
 	 * This is because they should not be accesible by other objects - said other objects will
 	 * be other entites within a given battle.*/
 protected:
-
+	/* I haven't yet decided on all of these.
+	 * More may be added or taken away in the future. */
+	std::string name;
+	int integrity = 0;
+	int initiative;
 public:
 	int action(); // Since the mainloop executes this function, it must be public.
 };
