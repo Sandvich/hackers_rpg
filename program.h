f47364d2f85c3b72910e9471ae91cf3ece136f07
Sanchit Sharma <ssharma@vpt.co.uk>
2016-07-25 12:31:46 +0100

    Minor reorganisation and additional commenting in program.h

--- program.h
+++ program.h
@@ -10,16 +10,19 @@ protected:
 	int function_count; //For remembering which function we call next
 	bool modify_integrity(int modifier);
 	void modify_initiative(int modifier);
-	/* Most player-defined functions should go here,
-	 * This is because they should not be accesible by other objects - said other objects will
-	 * be other entites within a given battle.*/
 	std::string name;
 	int integrity;
 	int initiative;
+	/* Most player-defined functions should go here,
+	 * This is because they should not be accesible by other objects - said other objects will
+	 * include other entites within a given battle.*/
 public:
 	Program(std::string name, int skill, int initiative);
 	int action(); // Since the mainloop executes this function, it must be public.
 	void attacked(int damage);
+	/* A few player-defined functions must go here - they will be explicitly mentioned
+	 * by the code blocks in question (particularly powerful blocks may insist on being
+	 * public to limit their usefulness.*/
 };
 
 #endif
